1b086141f56ad2bceda66cafacdb168b9bd7a240
Mattle <coin1994@outlook.com>
2019-10-29 12:27:39 +0800

    正则

--- shadow/src/main/java/com/coin/shadow/kits/RegexKits.java
+++ shadow/src/main/java/com/coin/shadow/kits/RegexKits.java
@@ -1,5 +1,8 @@
 package com.coin.shadow.kits;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,6 +13,7 @@ import java.util.regex.Pattern;
  * @modified By：孙伟
  * @version: v1.0.0.0
  */
+
 public final class RegexKits {
     /***
      * 禁止外部初始化
@@ -24,8 +28,72 @@ public final class RegexKits {
      * @return
      */
     public static boolean match(String regex, String target){
-        Pattern pattern = Pattern.compile(regex);
+        Pattern pattern = compile(regex, true);
         Matcher matcher = pattern.matcher(target);
         return matcher.matches();
+    }
+
+
+    public static String matchFirst(String regex, String target, int group){
+        Pattern pattern = compile(regex, true);
+        Matcher matcher = pattern.matcher(target);
+        while (matcher.find()){
+            return matcher.group(group).trim();
+        }
+        return "";
+    }
+
+    public static String matchFirst(String regex, String target, String name){
+        Pattern pattern = compile(regex, true);
+        Matcher matcher = pattern.matcher(target);
+        while (matcher.find()){
+            return matcher.group(name).trim();
+        }
+        return "";
+    }
+
+    public static List<String> matchGroup(String regex, String target, int group){
+        List<String> ret = Lists.newArrayList();
+        Pattern pattern = compile(regex, true);
+        Matcher matcher = pattern.matcher(target);
+        while (matcher.find()){
+            ret.add(matcher.group(group));
+        }
+        return ret;
+    }
+    public static List<String> matchGroup(String regex, String target, String name){
+        List<String> ret = Lists.newArrayList();
+        Pattern pattern = compile(regex, true);
+        Matcher matcher = pattern.matcher(target);
+        while (matcher.find()){
+            ret.add(matcher.group(name));
+        }
+        return ret;
+    }
+
+    /***
+     *
+     * @param regex  正则表达式
+     * @param target 匹配所有
+     * @return
+     */
+    public static List<String> matchAll(String regex, String target){
+        List<String> ret = Lists.newArrayList();
+        Pattern pattern = compile(regex, true);
+        Matcher matcher = pattern.matcher(target);
+        while (matcher.find()){
+            ret.add(matcher.group());
+        }
+        return ret;
+    }
+
+    /***
+     *
+     * @param regex 正则表达式
+     * @param ignore 是否区分大小写
+     * @return
+     */
+    public static Pattern compile(String regex, boolean ignore){
+        return  ignore ? Pattern.compile(regex) : Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
     }
 }
